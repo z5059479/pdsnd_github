aac8c7a672cf49b9da3b6a67efdeeaa8d962dfec
z5059479 <z5059479@student.unsw.edu.au>
2019-06-16 19:16:09 +1000

    add the second comment

--- bikeshare.py
+++ bikeshare.py
@@ -8,6 +8,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 #1st line
+#2nd line
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
